802bda854b5639fb0cccdb4d4bb27ee09521a7a1
BT User <bt_user@wipro.com>
2023-02-03 15:15:19 +0530

    Loader added

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -8,31 +8,35 @@ import axios from 'axios';
 export default function DropdownComp(props) {
     const format = data.format;
     const [input, setInput] = useState();
-    const display =  [{ value: "", text: "No data found", disabled: true }]
+    const [isLoading, setIsLoading] = useState(false);
+    const display = [{ value: "", text: "No data found", disabled: true }]
     const [newDisplay, setDisplay] = useState([])
     useEffect(() => {
-        if(format.dataSource === 'api'){
-        axios.get(format.url)
-            .then(resp => {
-                let newContent = [{ value: "", text: format.displayName, disabled: true }];
-                for(const key in resp.data){
-                    newContent.push({
-                        value: resp.data[key][`${format.responseSchema}`].toLowerCase(),
-                        text: resp.data[key][`${format.responseSchema}`]});
-                }
-                setDisplay(newContent);
-            })
+        if (format.dataSource === 'api') {
+            setIsLoading(true)
+            axios.get(format.url)
+                .then(resp => {
+                    let newContent = [{ value: "", text: format.displayName, disabled: true }];
+                    for (const key in resp.data) {
+                        newContent.push({
+                            value: resp.data[key][`${format.responseSchema}`].toLowerCase(),
+                            text: resp.data[key][`${format.responseSchema}`]
+                        });
+                    }
+                    setDisplay(newContent);
+                    setIsLoading(false);
+                })
                 .catch(err => console.log(err))
-        } else{
+        } else {
             setDisplay(display);
         }
-    }, [])
+    }, [format])
 
 
 
-    for(const item in newDisplay){
-        if(newDisplay[item].value.toLowerCase() === format.defaultValue)
-        newDisplay[item]={
+    for (const item in newDisplay) {
+        if (newDisplay[item].value.toLowerCase() === format.defaultValue)
+            newDisplay[item] = {
                 value: newDisplay[item].value,
                 text: newDisplay[item].text,
                 default: true
@@ -43,18 +47,21 @@ export default function DropdownComp(props) {
         <Grid divided="vertically">
             <Grid.Row columns={16}>
                 <Grid.Column width={format.width}>
-                        <Dropdown
-                            placeholder={format.displayName}
-                            fluid
-                            search
-                            selection
-                            options={newDisplay}
-                            required={format.required}
-                            disabled={!format.editable}
-                            onChange={(e, {value}) => {
-                                setInput(value.charAt(0).toUpperCase()+value.slice(1))} }
-                        />
-                        <p>{input && input}</p>
+                    <Dropdown
+                        placeholder={format.displayName}
+                        fluid
+                        search
+                        selection
+                        options={newDisplay}
+                        required={format.required}
+                        disabled={!format.editable}
+                        loading={isLoading}
+                        onChange={(e, { value }) => {
+                            setInput(value.charAt(0).toUpperCase() + value.slice(1))
+                        }}
+                    >
+                    </Dropdown>
+                    <p>{input && input}</p>
                 </Grid.Column>
             </Grid.Row>
         </Grid>
